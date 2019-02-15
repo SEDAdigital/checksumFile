c7e567f50ffaa4902404da2cf1782c1ed043da4a
Sebastian Clemens <sc@seda.digital>
2019-02-15 17:07:43 +0100

    Update hash algorithm, shorten not required anymore

--- core/components/checksumfile/elements/snippets/checksumFile.snippet.php
+++ core/components/checksumfile/elements/snippets/checksumFile.snippet.php
@@ -3,10 +3,8 @@ $file = $modx->getOption('file', $scriptProperties, '');
 if (empty($file)) return '';
 
 // calculate checksum
-$checksum = hash_file('adler32', MODX_BASE_PATH.$file);
+$checksum = hash_file('crc32b', MODX_BASE_PATH.$file);
 if (empty($checksum)) return $file;
-// shorten checksum https://stackoverflow.com/a/28602439
-$checksum = substr(base_convert(md5($checksum), 16,32), 0, 12);
 
 // add checksum to file url
 return substr_replace($file, '.'.$checksum.'.', strrpos($file, '.'), 1);
