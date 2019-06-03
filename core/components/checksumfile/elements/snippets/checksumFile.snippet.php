30739f24de108385de6449d698331afd3ab4734d
Thomas Jakobi <thomas.jakobi@partout.info>
2019-06-03 11:03:29 +0200

    Add a property for copying the file to the busted filename

--- core/components/checksumfile/elements/snippets/checksumFile.snippet.php
+++ core/components/checksumfile/elements/snippets/checksumFile.snippet.php
@@ -1,10 +1,27 @@
 <?php
 $file = $modx->getOption('file', $scriptProperties, '');
-if (empty($file)) return '';
+$copy = (bool)$modx->getOption('copy', $scriptProperties, 0);
+
+if (empty($file)) {
+    return '';
+}
 
 // calculate checksum
-$checksum = hash_file('crc32b', MODX_BASE_PATH.$file);
-if (empty($checksum)) return $file;
+$checksum = hash_file('crc32b', MODX_BASE_PATH . $file);
+if (empty($checksum)) {
+    return $file;
+}
 
 // add checksum to file url
-return substr_replace($file, '.'.$checksum.'.', strrpos($file, '.'), 1);
+$bustedName = substr_replace($file, '.' . $checksum . '.', strrpos($file, '.'), 1);
+
+if ($copy && !file_exists(MODX_BASE_PATH . $bustedName)) {
+    if (@copy(MODX_BASE_PATH . $file, MODX_BASE_PATH . $bustedName)) {
+        return $bustedName;
+    } else {
+        $modx->log(xPDO::LOG_LEVEL_ERROR, 'Can\'t copy ' . $file . ' to ' . $bustedName, '', 'checksumFile');
+        return $file;
+    }
+} else {
+    return $bustedName;
+}
